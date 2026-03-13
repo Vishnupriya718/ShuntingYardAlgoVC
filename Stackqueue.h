329fcc3b230dec85779cd0bee694a3d2ac594215
Vishnupriya Chunduri <vishnupriyachunduri@Vishnupriyas-MacBook-Air-7.local>
2026-03-13 09:46:28 -0700

    stack created successfully

---
+++ Stackqueue.h
@@ -0,0 +1,15 @@
+#ifndef STACKQUEUE_H
+#define STACKQUEUE_H
+
+#include "Node.h"
+#include <iostream>
+using namespace std;
+
+// STACK CLASS
+class Stack{
+public:
+    Node* top = nullptr;
+
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Stackqueue.h"
+
+using namespace std;
+
+int main(){
+
+    Stack s;
+
+    cout << "Stack created successfully" << endl;
+
+    return 0;
+}
