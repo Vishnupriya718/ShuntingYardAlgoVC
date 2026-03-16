03711809ba1cd3f787cf440578a406041c53b80a
Vishnupriya Chunduri <vishnupriyachunduri@Vishnupriyas-MacBook-Air-7.local>
2026-03-15 17:05:19 -0700

    precedence function

--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include "Stackqueue.h"
+
+// returns precedence of operators
+int precedence(char op){
+    if(op == '+' || op == '-') return 1;
+    if(op == '*' || op == '/') return 2;
+    if(op == '^') return 3;
+    return 0;
+}
 int main(){
 
     Stack s;
     Queue q;
 
     cout << "Queue created successfully" << endl;
-
+    cout << precedence('+') << endl;
+    cout << precedence('*') << endl;
+    cout << precedence('^') << endl;
     return 0;
 }
-
-   
-   
-
-   
